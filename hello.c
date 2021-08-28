08a6f900c8d06fd24232d2e84f856b8c5080fed1
Cailen Schlake <cschlake2@huskers.unl.edu>
2021-08-28 22:35:03 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Cailen Schlake
+ *         cschlake2@huskers.unl.edu
+ * Date: 2021/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Cailen Schlake!\n");
+  printf("Computer Science\n");
+
+  return 0;
+}
